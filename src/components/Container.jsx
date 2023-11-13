0e9a4de23c98076e585ca40b2424b1799d775c4d
GBalatti9 <gas.balatti@gmail.com>
2023-11-13 10:07:52 -0300

    File structure

---
+++ src/components/Container.jsx
@@ -0,0 +1,12 @@
+import { Form } from "./Form"
+import { Result } from "./Result"
+
+
+export const Container = () => {
+    return (
+        <>
+            <Form />
+            <Result />
+        </>
+    )
+}
