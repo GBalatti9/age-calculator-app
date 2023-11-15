2613842e0f8ddecc6c90f01635deb2aa515c2772
GBalatti9 <gas.balatti@gmail.com>
2023-11-15 10:30:23 -0300

    Implemented useRef to handle elements from the DOM The user has an interactive checks on the inputs. If he enters a number below 0 the input will turn his borders to red, else the borders will be green. Also the send button is goint to be display only if the three inputs have value

--- src/helpers/checkInputs.js
+++ src/helpers/checkInputs.js
@@ -1,63 +1,68 @@
-import { useState } from "react";
-
+import { useState, useEffect, useRef } from "react";
 
 export const checkInputs = () => {
-
-    let btn = document.getElementById('btn');
-    // btn.disabled = true;
-    // btn.style.backgroundColor = 'grey';
-    // btn.style.color = 'black';
-    // btn.style.opacity = '100%';
-
     const [errors, setErrors] = useState({
         day: '',
         month: '',
         year: '',
     });
 
-    const updateErrors = (obj, name, str) => {
-
-        setErrors({
-            ...obj,
-            [name]: str
-        })
-
-    }
-
-    const onCheckInputs = ({ target }) => {
-
-        const { name } = target;
-        console.log(name);
-        
-        if (name === 'day') {
-            const inputDay = document.getElementById('day');
-            let value = Number(inputDay.value)
-            inputDay.classList.remove('border-success', 'border-danger');
-            value > 0 ?
-            inputDay.classList.add('border', 'border-success'):
-            inputDay.classList.add('border', 'border-danger');
-        }
-        if (name === 'month') {
-            const inputMonth = document.getElementById('month');
-            let value = Number(inputMonth.value);
-            inputMonth.classList.remove('border-success', 'border-danger');
-            value > 0 ?
-            inputMonth.classList.add('border', 'border-success'):
-            inputMonth.classList.add('border', 'border-danger');
-        }
-        if (name === 'year') {
-            const inputYear = document.getElementById('year');
-            let value = Number(inputYear.value);
-            inputYear.classList.remove('border-success', 'border-danger');
-            value > 0 ?
-            inputYear.classList.add('border', 'border-success'):
-            inputYear.classList.add('border', 'border-danger');
+    const checkErrors = () => {
+        if (inputDay.current.classList.contains('border-danger') 
+            || inputMonth.current.classList.contains('border-danger')
+            || inputYear.current.classList.contains('border-danger') ) {
+            btn.current.disabled = true;
+        } else {
+            btn.current.disabled = false;
         }
     }
+    const inputDay = useRef(null);
+    const inputMonth = useRef(null);
+    const inputYear = useRef(null);
+    const btn = useRef(null);
+
+    useEffect(() => {
+        inputDay.current = document.getElementById('day');
+        inputMonth.current = document.getElementById('month');
+        inputYear.current = document.getElementById('year');
+        btn.current = document.getElementById('btn');
+
+        const onCheckInputs = ({ target }) => {
+            const { name } = target;
+            console.log(name);
+
+            if (name === 'day') {
+                let value = Number(inputDay.current.value)
+                inputDay.current.classList.remove('border-success', 'border-danger');
+                value > 0 ?
+                    inputDay.current.classList.add('border', 'border-success') :
+                    inputDay.current.classList.add('border', 'border-danger');
+                checkErrors();
+            }
+            if (name === 'month') {
+                let value = Number(inputMonth.current.value);
+                inputMonth.current.classList.remove('border-success', 'border-danger');
+                value > 0 ?
+                    inputMonth.current.classList.add('border', 'border-success') :
+                    inputMonth.current.classList.add('border', 'border-danger');
+                checkErrors();
+            }
+            if (name === 'year') {
+                let value = Number(inputYear.current.value);
+                inputYear.current.classList.remove('border-success', 'border-danger');
+                value > 0 ?
+                    inputYear.current.classList.add('border', 'border-success') :
+                    inputYear.current.classList.add('border', 'border-danger');
+                checkErrors();
+            }
+        };
+
+        inputDay.current.addEventListener('input', onCheckInputs);
+        inputMonth.current.addEventListener('input', onCheckInputs);
+        inputYear.current.addEventListener('input', onCheckInputs);
+    }, []);
 
     return {
-        onCheckInputs,
         errors,
-    }
-
-}
+    };
+};
